7173ff391466cf4866a9db79529a9050fc689287
ilovefafa <3373198511@qq.com>
2018-09-16 23:45:35 +0800

    save

--- app.js
+++ app.js
@@ -1,50 +1,54 @@
-
 //数据库配置
-require("./mongoose/index.js")
+require("./mongoose/index.js");
 //koa实例
-const Koa = require('koa');
+const Koa = require("koa");
 const app = new Koa();
 //跨域
-let cors = require('koa2-cors');
-app.use(cors({
-    origin: 'http://localhost:8080',
+let cors = require("koa2-cors");
+app.use(
+  cors({
+    origin: "http://localhost:8080",
     credentials: true
-}));
+  })
+);
 // 解析数据
-const koaBody = require('koa-body');
+const koaBody = require("koa-body");
 app.use(koaBody());
 // 配置静态资源
-const serve = require('koa-better-static2');
-app.use(serve('.'));
-app.use(serve('static/dist', { index: 'index.html' }));
+const serve = require("koa-better-static2");
+app.use(serve("."));
+app.use(serve("static/dist", { index: "index.html" }));
 //session
-app.keys = ['some secret hurr'];
-const session = require('./session/index');
+app.keys = ["some secret hurr"];
+const session = require("./session/index");
 const CONFIG = {
-    key: 'koa:sess', /** (string) cookie key (default is koa:sess) */
-    /** (number || 'session') maxAge in ms (default is 1 days) */
-    /** 'session' will result in a cookie that expires when session/browser is closed */
-    /** Warning: If a session cookie is stolen, this cookie will never expire */
-    maxAge: 86400000,
-    overwrite: true, /** (boolean) can overwrite or not (default true) */
-    httpOnly: true, /** (boolean) httpOnly or not (default true) */
-    signed: true, /** (boolean) signed or not (default true) */
-    rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
-    renew: true, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
+  key: "koa:sess" /** (string) cookie key (default is koa:sess) */,
+  /** (number || 'session') maxAge in ms (default is 1 days) */
+  /** 'session' will result in a cookie that expires when session/browser is closed */
+  /** Warning: If a session cookie is stolen, this cookie will never expire */
+  maxAge: 86400000,
+  overwrite: true /** (boolean) can overwrite or not (default true) */,
+  httpOnly: true /** (boolean) httpOnly or not (default true) */,
+  signed: true /** (boolean) signed or not (default true) */,
+  rolling: false /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */,
+  renew: true /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
 };
 app.use(session(CONFIG, app));
 app.use(async (ctx, next) => {
-    await next()
-    if (ctx.session.id) {
-        ctx.body.login = true
-    } else {
-        ctx.body.login = false
-    }
-})
+  ctx.body = {};
+  await next();
+  if (ctx.body.message === "session错误") {
+    ctx.session = null;
+    ctx.body.login = false;
+  } else if (ctx.session && ctx.session.id) {
+    ctx.body.login = true;
+  } else {
+    ctx.body.login = false;
+  }
+});
 
 //路由
-require("./route")(app)
-
+require("./route")(app);
 
 // //开启https服务
 // const fs = require('fs');
@@ -57,4 +61,4 @@ require("./route")(app)
 
 // https.createServer(options, app.callback()).listen(3002);
 
-app.listen(3002);
+app.listen(3005);
--- route/user.js
+++ route/user.js
@@ -1,133 +1,226 @@
-const Router = require('koa-router');
-const koaBody = require('koa-body');
-const User = require("../mongoose/model/user.js")
+const Router = require("koa-router");
+const koaBody = require("koa-body");
+const User = require("../mongoose/model/user.js");
 const NodeCache = require("node-cache");
-const uploadDir = './static/storage/img/userHeadImg'
+var bcrypt = require("bcrypt");
+const uploadDir = "./static/storage/img/userHeadImg";
 const authCodeCache = new NodeCache({ stdTTL: 60 * 60 * 5 });
 function sparateRouteFile(app) {
-    let router = new Router({
-        prefix: "/api/user"
-    })
+  let router = new Router({
+    prefix: "/api/user"
+  });
 
-    router.post(
-        '/upLoadHeadPicture',
-        koaBody({
-            multipart: true,
-            formidable: {
-                uploadDir: uploadDir,
-            }
-        }),
-        async (ctx) => {
-            let file = ctx.request.body.files.file
-            let id = ctx.session.id
-            let removeUploadDir = uploadDir.substring(2) + '/'
-            let getDir = '/' + uploadDir.substring(2) + '/'
-            let fileName = file.path.replace(/\\/g, '/').replace(removeUploadDir, '')
-            await User.update({ _id: id }, { 'userInfo.headImg': fileName })
-            ctx.body = {
-                code: 200,
-                data: {
-                    headImg: getDir + fileName
-                }
-            };
+  router.post(
+    "/upLoadHeadPicture",
+    koaBody({
+      multipart: true,
+      formidable: {
+        uploadDir: uploadDir
+      }
+    }),
+    async ctx => {
+      let file = ctx.request.body.files.file;
+      let id = ctx.session.id;
+      let removeUploadDir = uploadDir.substring(2) + "/";
+      let getDir = "/" + uploadDir.substring(2) + "/";
+      let fileName = file.path.replace(/\\/g, "/").replace(removeUploadDir, "");
+      let headImg = getDir + fileName;
+      await User.update({ _id: id }, { "userInfo.headImg": headImg });
+      ctx.body = {
+        code: 200,
+        data: {
+          headImg
         }
+      };
+    }
+  );
+
+  router.post("/name", async ctx => {
+    let name = ctx.request.body.name;
+    let id = ctx.session.id;
+    await User.update(
+      { _id: id },
+      { "userInfo.name": name, "userInfo.changeName": true }
     );
+    ctx.body = {
+      code: 200,
+      data: {
+        name,
+        changeName: true
+      }
+    };
+  });
 
-    router.post('/name', async (ctx) => {
-        let name = ctx.request.body.name
-        let id = ctx.session.id
-        await User.update({ _id: id }, { 'userInfo.name': name, 'userInfo.changeName': true })
-        ctx.body = {
-            code: 200,
-            data: {
-                name,
-                changeName: true
-            }
-        };
-    })
+  router.post("/authCode", async ctx => {
+    let id = ctx.session.id;
+    let find = await User.findOne({ _id: id });
+    let phoneNumber = find.phoneNumber;
+    let authCode = (figures => {
+      let randomNumber = "";
+      for (let i = 0; i < figures; i++) {
+        randomNumber += Math.floor(Math.random() * 10).toString();
+      }
+      return randomNumber;
+    })(6);
+    await authCodeCache.set(phoneNumber, authCode);
+    ctx.body = {
+      code: 200,
+      data: {
+        authCode: "验证码为:" + authCode + ",有效时间5分钟"
+      }
+    };
+  });
 
-    router.post('/authCode', async (ctx) => {
-        let id = ctx.session.id
-        let find = await User.findOne({ _id: id })
-        let phoneNumber = find.phoneNumber
-        let authCode = ((figures) => {
-            let randomNumber = ''
-            for (let i = 0; i < figures; i++) {
-                randomNumber += Math.floor(Math.random() * 10).toString()
-            }
-            return randomNumber
-        })(6)
-        await authCodeCache.set(phoneNumber, authCode)
-        ctx.body = {
-            code: 200,
-            data: {
-                authCode: '验证码为:' + authCode + ',有效时间5分钟'
-            }
-        };
-    })
+  router.post("/confirmAuthCode", async ctx => {
+    let authCode = ctx.request.body.authCode;
+    let id = ctx.session.id;
+    let find = await User.findOne({ _id: id });
+    let phoneNumber = find.phoneNumber;
+    let getCache = await authCodeCache.get(phoneNumber);
+    if (getCache === authCode) {
+      ctx.body = {
+        code: 200,
+        data: {}
+      };
+    } else {
+      ctx.body = {
+        code: 400,
+        errMessage: "验证码错误"
+      };
+    }
+  });
 
-    router.post('/confirmAuthCode', async (ctx) => {
-        let authCode = ctx.request.body.authCode
-        let id = ctx.session.id
-        let find = await User.findOne({ _id: id })
-        let phoneNumber = find.phoneNumber
-        let getCache = await authCodeCache.get(phoneNumber)
-        if (getCache === authCode) {
-            ctx.body = {
-                code: 200,
-                data: {
-                }
-            };
-        } else {
-            ctx.body = {
-                code: 400,
-                errMessage: '验证码错误',
-            };
+  router.post("/newPhoneAuthCode", async ctx => {
+    let phoneNumber = ctx.request.body.phoneNumber;
+    let authCode = (figures => {
+      let randomNumber = "";
+      for (let i = 0; i < figures; i++) {
+        randomNumber += Math.floor(Math.random() * 10).toString();
+      }
+      return randomNumber;
+    })(6);
+    await authCodeCache.set(phoneNumber, authCode);
+    ctx.body = {
+      code: 200,
+      data: {
+        authCode: "验证码为:" + authCode + ",有效时间5分钟"
+      }
+    };
+  });
+
+  router.post("/newPhoneConfirmAuthCode", async ctx => {
+    let authCode = ctx.request.body.authCode;
+    let phoneNumber = ctx.request.body.phoneNumber;
+    let encryptPhoneNumber = `${phoneNumber.slice(0, 3)}****${phoneNumber.slice(
+      -4
+    )}`;
+    let id = ctx.session.id;
+    let getCache = await authCodeCache.get(phoneNumber);
+    if (getCache === authCode) {
+      await User.update(
+        { _id: id },
+        { phoneNumber, "userInfo.encryptPhoneNumber": encryptPhoneNumber }
+      );
+      ctx.body = {
+        code: 200,
+        data: {
+          encryptPhoneNumber
         }
-    })
+      };
+    } else {
+      ctx.body = {
+        code: 400,
+        errMessage: "验证码错误"
+      };
+    }
+  });
 
-    router.post('/newPhoneAuthCode', async (ctx) => {
-        let phoneNumber = ctx.request.body.phoneNumber
-        let authCode = ((figures) => {
-            let randomNumber = ''
-            for (let i = 0; i < figures; i++) {
-                randomNumber += Math.floor(Math.random() * 10).toString()
-            }
-            return randomNumber
-        })(6)
-        await authCodeCache.set(phoneNumber, authCode)
+  router.post("/updatePassword", async ctx => {
+    let { newPassword, oldPassword } = ctx.request.body;
+    let id = ctx.session.id;
+    let find = await User.findOne({ _id: id });
+    if (find.password) {
+      let compare = await bcrypt.compare(oldPassword, find.password);
+      if (compare) {
+        let password = await bcrypt.hash(newPassword, 10);
+        await User.update({ _id: id }, { password });
         ctx.body = {
-            code: 200,
-            data: {
-                authCode: '验证码为:' + authCode + ',有效时间5分钟'
-            }
+          code: 200
         };
-    })
+      } else {
+        ctx.body = {
+          code: 400,
+          message: "旧密码错误"
+        };
+        return;
+      }
+    }
+    let password = await bcrypt.hash(newPassword, 10);
+    await User.update({ _id: id }, { password });
+    ctx.body = {
+      code: 200
+    };
+  });
+  router.post("/logout", async ctx => {
+    ctx.session = null;
+    ctx.body = {
+      code: 200
+    };
+  });
 
-    router.post('/newPhoneConfirmAuthCode', async (ctx) => {
-        let authCode = ctx.request.body.authCode
-        let phoneNumber = ctx.request.body.phoneNumber
-        let encryptPhoneNumber = `${phoneNumber.slice(0, 3)}****${phoneNumber.slice(-4)}`
-        let id = ctx.session.id
-        let getCache = await authCodeCache.get(phoneNumber)
-        if (getCache === authCode) {
-            await User.update({ _id: id }, { phoneNumber, 'userInfo.encryptPhoneNumber': encryptPhoneNumber })
-            ctx.body = {
-                code: 200,
-                data: {
-                    encryptPhoneNumber
-                }
-            };
-        } else {
-            ctx.body = {
-                code: 400,
-                errMessage: '验证码错误',
-            };
-        }
-    })
+  router.post("/address", async ctx => {
+    let data = ctx.request.body;
+    let _id = ctx.session.id;
+    await User.update(
+      { _id },
+      { $push: { "userInfo.addresses": { ...data } } }
+    );
+    ctx.body = {
+      code: 200
+    };
+  });
 
-    app.use(router.routes());
-    app.use(router.allowedMethods());
+  router.get("/address", async ctx => {
+    let _id = ctx.session.id;
+    let find = await User.findOne({ _id });
+    ctx.body = {
+      code: 200,
+      data: {
+        addresses: find.userInfo.addresses
+      }
+    };
+  });
+
+  router.delete("/address", async ctx => {
+    let addressID = ctx.request.query._id;
+    let _id = ctx.session.id;
+    let updateInfo = await User.update(
+      { _id },
+      { $pull: { "userInfo.addresses": { _id: addressID } } }
+    );
+    ctx.body = {
+      code: 200,
+      data: {
+        updateInfo
+      }
+    };
+  });
+
+  router.put("/address", async ctx => {
+    let data = ctx.request.body;
+    let addressID = data._id;
+    let _id = ctx.session.id;
+    await User.update(
+      { _id, "userInfo.addresses._id": addressID },
+      { "userInfo.addresses.$": { ...data } }
+    );
+    ctx.body = {
+      code: 200
+    };
+  });
+
+  app.use(router.routes());
+  app.use(router.allowedMethods());
 }
 
-module.exports = sparateRouteFile
+module.exports = sparateRouteFile;
